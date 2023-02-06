5a585a60156be43f6f254f0027be406aafc656c9
Simi <simi.sreenivas@gmail.com>
2023-02-06 15:15:55 +0000

    installed react-router-dom and set up header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <NavBar/>
+      <Routes>
+        <Route>
+          
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+
+const Header = () => {
+  return (
+    <h1>Welcome to NC Games!</h1>
+  )
+}
+
+export default Header;
